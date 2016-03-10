ea9ef77cfcfa63e39aa3fa1bc833d2cfca344bc8
Maria <mmamonova95@gmail.com>
2016-03-10 21:01:46 +0300

    added operator overloading

--- ConsoleApplications/ConsoleApplication3/Program.cs
+++ ConsoleApplications/ConsoleApplication3/Program.cs
@@ -8,8 +8,91 @@ namespace ConsoleApplication3
 {
     class Program
     {
+		struct Point
+		{
+			public int x;
+			public int y;
+			public int z;
+
+			public Point(int X, int Y, int Z)
+			{
+				x = X;
+				y = Y;
+				z = Z;
+			}
+
+			public static Point operator +(Point point1, Point point2)
+			{
+				Point resultPoint;
+				resultPoint.x = point1.x + point2.x;
+				resultPoint.y = point1.y + point2.y;
+				resultPoint.z = point1.z + point2.z;
+				return resultPoint;
+			}
+			public static Point operator -(Point point1, Point point2)
+			{
+				Point resultPoint;
+				resultPoint.x = point1.x - point2.x;
+				resultPoint.y = point1.y - point2.y;
+				resultPoint.z = point1.z - point2.z;
+				return resultPoint;
+			}
+
+		}
+
         static void Main(string[] args)
         {
-        }
+			Point point1;
+			Point point2;
+			int X1, X2, Y1, Y2, Z1, Z2;
+
+			Console.WriteLine("Enter coordinates of the point1");
+			do
+			{
+				try
+				{
+					X1 = Convert.ToInt32(Console.ReadLine());
+					Y1 = Convert.ToInt32(Console.ReadLine());
+					Z1 = Convert.ToInt32(Console.ReadLine());
+
+					break;
+				}
+				catch (System.FormatException)
+				{
+					Console.WriteLine("ERROR: You have entered a symbol, not a number. Try enter point again");
+				}
+			}
+			while (true);
+			point1 = new Point(X1, Y1, Z1);
+
+			Console.WriteLine("Enter coordinates of the point2");
+			do
+			{
+				try
+				{
+					X2 = Convert.ToInt32(Console.ReadLine());
+					Y2 = Convert.ToInt32(Console.ReadLine());
+					Z2 = Convert.ToInt32(Console.ReadLine());
+
+					break;
+				}
+				catch (System.FormatException)
+				{
+					Console.WriteLine("ERROR: You have entered a symbol, not a number. Try enter point again");
+				}
+			}
+			while (true);
+			point2 = new Point(X2, Y2, Z2);
+
+			Point resultPoint = point1 + point2;
+			Print(resultPoint);
+
+			Console.ReadKey();
+		}
+
+		static void Print(Point p)
+		{
+			Console.WriteLine("Point is ({0}; {1}; {2})", p.x, p.y, p.z);
+		}
     }
 }
