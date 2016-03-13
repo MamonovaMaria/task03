59203cf1c2cab11a25f0ac18f66761bef3b74aa5
Maria <mmamonova95@gmail.com>
2016-03-13 15:27:14 +0300

    added extension method

---
+++ ConsoleApplications/ConsoleApplication5/MyExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+	static class MyExtension
+	{
+		public static string ConvertListToString<T>(this IList<T> list)
+		{
+			StringBuilder r = new StringBuilder("");
+
+			for (int i = 0; i < list.Count - 1; i++)
+				r = r.AppendFormat("{0}, ", list[i]);
+			r.Append(list[list.Count-1]);
+
+			return r.ToString();
+		}
+	}
+}
